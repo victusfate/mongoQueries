f8dc6dd42d043612be4d4ab8ea8f89fdb669eef8
Ubuntu <ubuntu@development-script.(none)>
2012-08-07 11:39:11 +0000

    checking for properties before looping them

--- findDynamicPostMediaVideoDump.js
+++ findDynamicPostMediaVideoDump.js
@@ -4,13 +4,19 @@ t = db.getMongo().getDB('cameo-development')
 t.full_scenes.find( 
 	function() { 
 		for (frames in this) { 
-			for (frame in this[frames]) { 
-				for (item in this[frames][frame]) { 
-					if (this[frames][frame][item].type == 'Dynamic.Post.Media.Video') { 
-						return true; 
-					} 
+			if (this[frames]) {
+				for (frame in this[frames]) { 
+					if (this[frames][frame]) {
+						for (item in this[frames][frame]) { 
+							if (this[frames][frame][item]) {
+								if (this[frames][frame][item].type == 'Dynamic.Post.Media.Video') { 
+									return true; 
+								} 
+							}
+						} 
+					}
 				} 
-			} 
+			}
 		} 
 		return false; 
 	}
